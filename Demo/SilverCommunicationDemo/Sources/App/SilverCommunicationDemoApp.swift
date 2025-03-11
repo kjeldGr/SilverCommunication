5ac656c7de895e7a2f9499d412bef24200abd75a
Kjeld Groot <kjeldgroot@gmail.com>
2025-03-11 17:23:07 +0100

    Add Demo Xcode project to demo SilverCommunication

---
+++ Demo/SilverCommunicationDemo/Sources/App/SilverCommunicationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverCommunicationDemoApp.swift
+//  SilverCommunicationDemo
+//
+//  Created by KPGroot on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SilverCommunicationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
