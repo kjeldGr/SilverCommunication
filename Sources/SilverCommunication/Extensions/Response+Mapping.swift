d2e0fc734079f1e3169948582ccb07c23e58bc0e
Kjeld Groot <kjeldgroot@gmail.com>
2025-07-14 12:32:26 +0200

    Add unwrap for Response with optional content type

--- Sources/SilverCommunication/Extensions/Response+Mapping.swift
+++ Sources/SilverCommunication/Extensions/Response+Mapping.swift
@@ -10,3 +10,14 @@ public extension Response {
         try Response<T>(statusCode: statusCode, headers: headers, content: transform(content))
     }
 }
+
+public extension Response {
+    func unwrap<T>() throws -> Response<T> where T? == ContentType {
+        try map { content in
+            guard let content else {
+                throw ValueError.missingValue(context: ValueError.Context(keyPath: \Response<T>.content))
+            }
+            return content
+        }
+    }
+}
